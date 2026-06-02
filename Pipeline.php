46a6e24fd1095a172012e59529903a77b0be5d75
root <action@github.com>
2026-06-02 16:24:55 +0000

    Remove workflow_run

---
+++ Pipeline.php
@@ -0,0 +1,224 @@
+<?php declare(strict_types=1);
+
+namespace StellarWP\Foundation\Pipeline;
+
+use Closure;
+use RuntimeException;
+use StellarWP\Foundation\Container\Contracts\Container;
+use Throwable;
+
+/**
+ * Adapted from Laravel's Pipeline to use our container system.
+ */
+class Pipeline
+{
+	/**
+	 * The container implementation.
+	 */
+	protected ?Container $container;
+
+	/**
+	 * The object being passed through the pipeline.
+	 */
+	protected mixed $passable;
+
+	/**
+	 * The array of class pipes.
+	 *
+	 * @var mixed[]
+	 */
+	protected array $pipes = [];
+
+	/**
+	 * The method to call on each pipe.
+	 */
+	protected string $method = 'handle';
+
+	/**
+	 * Create a new class instance.
+	 */
+	public function __construct(?Container $container = null) {
+		$this->container = $container;
+	}
+
+	/**
+	 * Set the container instance.
+	 *
+	 * @return $this
+	 */
+	public function setContainer(Container $container): self {
+		$this->container = $container;
+
+		return $this;
+	}
+
+	/**
+	 * Set the object being sent through the pipeline.
+	 *
+	 * @return $this
+	 */
+	public function send(mixed $passable): self {
+		$this->passable = $passable;
+
+		return $this;
+	}
+
+	/**
+	 * Set the array of pipes.
+	 *
+	 * @param array|mixed $pipes
+	 *
+	 * @return $this
+	 */
+	public function through(mixed $pipes): self {
+		$this->pipes = is_array($pipes) ? $pipes : func_get_args();
+
+		return $this;
+	}
+
+	/**
+	 * Push additional pipes onto the pipeline.
+	 *
+	 * @param array|mixed $pipes
+	 *
+	 * @return $this
+	 */
+	public function pipe(mixed $pipes): self {
+		array_push($this->pipes, ...(is_array($pipes) ? $pipes : func_get_args()));
+
+		return $this;
+	}
+
+	/**
+	 * Set the method to call on the pipes.
+	 */
+	public function via(string $method): self {
+		$this->method = $method;
+
+		return $this;
+	}
+
+	/**
+	 * Run the pipeline with a final destination callback.
+	 */
+	public function then(Closure $destination): mixed {
+		$pipeline = array_reduce(
+			array_reverse($this->pipes()), $this->carry(), $this->prepareDestination($destination)
+		);
+
+		return $pipeline($this->passable);
+	}
+
+	/**
+	 * Run the pipeline and return the result.
+	 */
+	public function thenReturn(): mixed {
+		return $this->then(fn ($passable) => $passable);
+	}
+
+	/**
+	 * Get the final piece of the Closure onion.
+	 */
+	protected function prepareDestination(Closure $destination): Closure {
+		return function ($passable) use ($destination) {
+			try {
+				return $destination($passable);
+			} catch (Throwable $e) {
+				return $this->handleException($passable, $e);
+			}
+		};
+	}
+
+	/**
+	 * Get a Closure that represents a slice of the application onion.
+	 */
+	protected function carry(): Closure {
+		return function ($stack, $pipe) {
+			return function ($passable) use ($stack, $pipe) {
+				try {
+					if (is_callable($pipe)) {
+						// If the pipe is a callable, then we will call it directly, but otherwise we
+						// will resolve the pipes out of the dependency container and call it with
+						// the appropriate method and arguments, returning the results back out.
+						return $pipe($passable, $stack);
+					} elseif (! is_object($pipe)) {
+						[$name, $parameters] = $this->parsePipeString($pipe);
+
+						// If the pipe is a string we will parse the string and resolve the class out
+						// of the dependency injection container. We can then build a callable and
+						// execute the pipe function giving in the parameters that are required.
+						$pipe = $this->getContainer()->get($name);
+
+						$parameters = array_merge([$passable, $stack], $parameters);
+					} else {
+						// If the pipe is already an object we'll just make a callable and pass it to
+						// the pipe as-is. There is no need to do any extra parsing and formatting
+						// since the object we're given was already a fully instantiated object.
+						$parameters = [$passable, $stack];
+					}
+
+					$carry = method_exists($pipe, $this->method)
+						? $pipe->{$this->method}(...$parameters)
+						: $pipe(...$parameters);
+
+					return $this->handleCarry($carry);
+				} catch (Throwable $e) {
+					return $this->handleException($passable, $e);
+				}
+			};
+		};
+	}
+
+	/**
+	 * Parse full pipe string to get name and parameters.
+	 *
+	 * @return array<int, mixed>
+	 */
+	protected function parsePipeString(string $pipe): array {
+		[$name, $parameters] = array_pad(explode(':', $pipe, 2), 2, []);
+
+		if (is_string($parameters)) {
+			$parameters = explode(',', $parameters);
+		}
+
+		return [$name, $parameters];
+	}
+
+	/**
+	 * Get the array of configured pipes.
+	 *
+	 * @return mixed[]
+	 */
+	protected function pipes(): array {
+		return $this->pipes;
+	}
+
+	/**
+	 * Get the container instance.
+	 *
+	 * @throws \RuntimeException
+	 */
+	protected function getContainer(): Container {
+		if (! $this->container) {
+			throw new RuntimeException('A container instance has not been passed to the Pipeline.');
+		}
+
+		return $this->container;
+	}
+
+	/**
+	 * Handle the value returned from each pipe before passing it to the next.
+	 */
+	protected function handleCarry(mixed $carry): mixed {
+		return $carry;
+	}
+
+	/**
+	 * Handle the given exception.
+	 *
+	 * @throws Throwable
+	 */
+	protected function handleException(mixed $passable, Throwable $e): mixed {
+		throw $e;
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Foundation Pipeline
+
+> [!WARNING]
+> **This is a read-only repository!** For pull requests or issues, see [stellarwp/foundation](https://github.com/stellarwp/foundation).
+
+A Pipeline / Command Design Pattern implementation based on [Laravel's Pipeline implementation](https://github.com/illuminate/pipeline/blob/master/Pipeline.php).
+
+## Installation
+
+```shell
+composer require stellarwp/foundation-pipeline
+```
